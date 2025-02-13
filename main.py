f99fd35add54342a90fe4a2c91401994252a016d
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2025-02-13 12:39:22 -0500

    initial implementation of mock api

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+# other imports
+import os
+import json
+from fastapi import FastAPI, HTTPException, UploadFile, File
+import aiofiles
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Message": "Hello World"}
+
+DATA_DIR = 'test_data'
+
+@app.post("/evaluate/mock/{id}")
+async def output(id: int, file: UploadFile = File(...)):
+    # verify file type
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="Uploaded file is not an image.")
+    
+    # get filename
+    filename = os.path.join(DATA_DIR, f"{id}.json")
+    # Check if the JSON file exists; if not, raise a 404 error.
+    if not os.path.exists(filename):
+        raise HTTPException(status_code=404, detail="JSON file not found")
+
+    # read json data
+    async with aiofiles.open(filename, mode='r') as f:
+        contents = await f.read()
+        data = json.loads(contents)
+    
+    return data
